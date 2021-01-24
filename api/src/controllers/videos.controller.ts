b962110e84b32878321a7365d4782a3b6f6843f5
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-01-23 19:30:52 -0600

    Api completed

--- api/src/controllers/videos.controller.ts
+++ api/src/controllers/videos.controller.ts
@@ -1,26 +1,36 @@
-import { RequestHandler, response } from 'express'
-import * as responses from '../res/responses'
+import { RequestHandler } from 'express'
+import * as response from '../res/responses'
 import videoModel from '../models/video.model'
 import chalk from 'chalk'
 
 export const getVideos: RequestHandler = async (req, res) => {
 	const databaseResponse = await videoModel.find()
-	responses.success(req, res, databaseResponse, 200)
+	response.success(req, res, databaseResponse, 200)
 }
 
 export const getVideo: RequestHandler = async (req, res) => {
-	console.log(req.params.id)
-	const databaseResponse = await videoModel.findById(req.params.id)
-	if (!databaseResponse)
-		return responses.failure(
-			req,
-			res,
-			'No se encontró el video:(',
-			500,
-			'[videos:controller]: no se encontró el video pedido',
+	try {
+		console.log(req.params.id)
+		const databaseResponse = await videoModel.findById(req.params.id)
+		if (!databaseResponse)
+			return response.failure(
+				req,
+				res,
+				'No se encontró el video:(',
+				500,
+				'[videos:controller]: no se encontró el video pedido',
+			)
+		console.log(databaseResponse)
+		response.success(req, res, databaseResponse, 200)
+	} catch (error) {
+		console.log(
+			chalk.red(
+				`${chalk.inverse.red(
+					'[Video controller]',
+				)} ha ocurrido un error interno: ${error}`,
+			),
 		)
-	console.log(databaseResponse)
-	responses.success(req, res, databaseResponse, 200)
+	}
 }
 
 export const createVideo: RequestHandler = async (req, res) => {
@@ -29,7 +39,7 @@ export const createVideo: RequestHandler = async (req, res) => {
 			url: req.body.url,
 		})
 		if (isAlreadyThisVideoInDatabase) {
-			return responses.failure(
+			return response.failure(
 				req,
 				res,
 				'El video ya existe en la base de datos',
@@ -38,7 +48,7 @@ export const createVideo: RequestHandler = async (req, res) => {
 		} else {
 			const video = new videoModel(req.body)
 			const savedVideo = await video.save()
-			responses.success(req, res, savedVideo, 201)
+			response.success(req, res, savedVideo, 201)
 		}
 	} catch (error) {
 		console.log(chalk.red(error))
@@ -46,5 +56,52 @@ export const createVideo: RequestHandler = async (req, res) => {
 }
 
 export const deleteVIdeo: RequestHandler = async (req, res) => {
-	res.send('saludos desde delete')
+	try {
+		const dataBaseResponse = await videoModel.findByIdAndDelete(req.params.id)
+		if (!dataBaseResponse) {
+			return response.failure(
+				req,
+				res,
+				'Error interno no pudimos eliminar el mensaje',
+				500,
+				'Ha ocurrido un error en el controlador de video al momento de eliminar un video',
+			)
+		}
+		return response.success(req, res, dataBaseResponse, 200)
+	} catch (error) {
+		console.log(
+			chalk.red(
+				`${chalk.inverse.red(
+					'[Video controller]',
+				)} ha ocurrido un error interno: ${error}`,
+			),
+		)
+	}
+}
+
+export const updateVideo: RequestHandler = async (req, res) => {
+	try {
+		const dataBaseResponse = await videoModel.findByIdAndUpdate(
+			req.params.id,
+			req.body,
+			{ new: true },
+		)
+		if (!dataBaseResponse)
+			return response.failure(
+				req,
+				res,
+				'Error interno al actualizar video',
+				204,
+				'Ha ocurrido un error al actualizar video en el controlador de videos',
+			)
+		return response.success(req, res, dataBaseResponse, 200)
+	} catch (error) {
+		console.log(
+			chalk.red(
+				`${chalk.inverse.red(
+					'[Video controller]',
+				)} ha ocurrido un error interno: ${error}`,
+			),
+		)
+	}
 }
