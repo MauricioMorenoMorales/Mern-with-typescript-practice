6e61d2990d767937277766af872d21aa92b1bfc6
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-07-01 16:11:34 -0500

    No sé

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+	return (
+		<div>
+			hello
+			<p>world</p>
+		</div>
+	)
+}
+
+export default App
---
+++ frontend/src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import './sass/index.sass'
+
+//Components
+import Header from './components/Header/Header'
+
+//Containers
+import Home from './containers/Home'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+ReactDOM.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Header />
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/app" component={App} />
+			</Switch>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById('root'),
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
